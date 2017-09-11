4b8e7ddacc8d43ff54c682c773f63089dfca2ee1
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2017-09-11 15:05:58 +0800

    m

--- app/assets/javascripts/video_plyr.js
+++ app/assets/javascripts/video_plyr.js
@@ -1,7 +1,11 @@
 $(document).on("turbolinks:load", function() {
   var full_width = $(window).width();
   if ($("#woc").get(0)) {
-    $('.plyrr').css('width', 950);
+    if (full_width > 1200){
+      $('.plyrr').css('width', 950);
+    } else {
+      $('.plyrr').css('width', full_width);      
+    }
   } else {
     if (full_width > 1200){
       $('.plyrr').css('width', 700);
