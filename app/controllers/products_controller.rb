cb22165d30bc3796b6d290669535df4061a64b26
jeffhappily <goodboy96@live.com.my>
2017-10-16 21:42:07 +0800

    Add a controller action for exact match modal

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,13 @@
 class ProductsController < ApplicationController
-  def show
+  before_action :set_product, only: [:show, :exact_match]
+
+  def show; end
+
+  def exact_match; end
+
+  private
+
+  def set_product
     @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :products, only: [:show]
+  resources :products, only: [:show] do
+    member do
+      get :exact_match
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "videos#index"
