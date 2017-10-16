a2de4db310c07b904c89c73d399edc7bd3284915
jeffhappily <goodboy96@live.com.my>
2017-10-17 07:05:07 +0800

    Add exact request table

---
+++ app/models/exact_request.rb
@@ -0,0 +1,4 @@
+class ExactRequest < ApplicationRecord
+  belongs_to :view
+  belongs_to :product
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :video
   has_many :clicks
+  has_many :exact_requests
 end
