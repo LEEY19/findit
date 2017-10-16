7907c8b4883a67ab20b10cbedc410a1f4eb6df2f
jeffhappily <goodboy96@live.com.my>
2017-10-16 20:49:29 +0800

    Change tooltip position

--- app/assets/javascripts/hover_products.js
+++ app/assets/javascripts/hover_products.js
@@ -1,7 +1,21 @@
 $(document).on("turbolinks:load", function() {
-  $(".product-list-row").hover(function(){
-    $(this).css("background-color", "#e6e6e6");
-    }, function(){
-    $(this).css("background-color", "white");
-  });
+  // $(".product-list-row").hover(function(){
+  //   $(this).css("background-color", "#e6e6e6");
+  //   }, function(){
+  //   $(this).css("background-color", "white");
+  // });
+  $(".productlist-row").on("mouseenter", function () {
+    $(".tooltip").html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
+    $.get('/products/' + $(this).data("product-id"));
+    $(".tooltip").css('opacity', 1);
+  })
+  $(".productlist-row").on("mouseleave", function (e) {
+    var el = e.toElement || e.relatedTarget;
+    if (!($(el).hasClass("tooltip") || $(el).parents(".tooltip").length))
+      // debugger
+      $(".tooltip").css('opacity', 0);
+  })
+  $(".tooltip").on("mouseleave", function () {
+    $(this).css('opacity', 0);
+  })
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  resources :products, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "videos#index"
   resources :videos
-  post "videos/click_product" => "videos#click_product" 
-  post '/track_scroll' => "videos#track_scroll" 
-  post '/register_category_click' => "videos#register_category_click" 
-  post '/record_session_duration' => "videos#record_session_duration" 
-  post '/update_session_time' => "videos#update_session_time" 
+  post "videos/click_product" => "videos#click_product"
+  post '/track_scroll' => "videos#track_scroll"
+  post '/register_category_click' => "videos#register_category_click"
+  post '/record_session_duration' => "videos#record_session_duration"
+  post '/update_session_time' => "videos#update_session_time"
   post '/track_toast_click' => "videos#track_toast_click"
   get 'videosm/:id' => "videos#show_manual", as: "show_manual"
   get 'videos/woc/:id' => "videos#woc", as: "woc"
