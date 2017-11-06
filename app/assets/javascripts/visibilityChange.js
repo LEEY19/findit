cb5039144cda47035b16a4b0f29dcd06ebd2ac03
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2017-11-07 01:34:29 +0800

    m

--- app/assets/javascripts/visibilityChange.js
+++ app/assets/javascripts/visibilityChange.js
@@ -3,8 +3,9 @@ var triggered = false
 $(document).on("turbolinks:load", function() {
 
 
-  var hidden, visibilityChange;
-  if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+  var hidden = null 
+  var visibilityChange = null
+  if (typeof document.hidden !== "undefined") {
     hidden = "hidden";
     visibilityChange = "visibilitychange";
   } else if (typeof document.msHidden !== "undefined") {
@@ -26,13 +27,11 @@ $(document).on("turbolinks:load", function() {
         url:'/record_session_duration/',
         data: { view_id : $("#view_id").text() },
         success:function(){
-          //I assume you want to do something on controller action execution success?
           console.log("success");
         }
       });
       console.log("gone")
     } else if (!document[hidden] && !triggered) {
-    // } else {
       triggered = true
       setTimeout(() => {
         triggered = false
@@ -42,7 +41,6 @@ $(document).on("turbolinks:load", function() {
         url:'/update_session_time/',
         data: { view_id : $("#view_id").text() },
         success:function(){
-          //I assume you want to do something on controller action execution success?
           console.log("success");
         }
       });
@@ -54,7 +52,6 @@ $(document).on("turbolinks:load", function() {
   if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
     console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
   } else {
-    // Handle page visibility change
     document.addEventListener(visibilityChange, handleVisibilityChange, false);
   }
 });
