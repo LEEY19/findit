9942341f70ed08871d9c0477575b429e42b3f40c
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2017-11-10 20:35:48 +0800

    m

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module Findit
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.time_zone = 'Singapore' 
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
